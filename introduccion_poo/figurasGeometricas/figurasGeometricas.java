a6a192ce6bc8abd67e412d659d4a3ff7444662ca
LJossue <101231796+LJossue@users.noreply.github.com>
2023-05-08 18:09:35 -0600

    Update figurasGeometricas.java

--- introduccion_poo/figurasGeometricas/figurasGeometricas.java
+++ introduccion_poo/figurasGeometricas/figurasGeometricas.java
@@ -1,6 +1,6 @@
-package figurasGeometricas;
+package figurasgeometricas;
 
-public abstract class figurasGeometricas {
+public abstract class figurasGeometricas{
   public abstract double calcularPerimetro();
   public abstract double calcularArea();
   
